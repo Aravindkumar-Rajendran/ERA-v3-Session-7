de573148f083eebfd3e934e9593caea9a754598c
Aravindkumar-rajendran <aravindrajendran1997@gmail.com>
2024-12-28 17:55:41 +0530

    training logs in ec2

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ if cuda:
     torch.cuda.manual_seed(SEED)
 
 # dataloader arguments - something you'll fetch these from cmdprmt
-dataloader_args = dict(shuffle=True, batch_size=128, num_workers=4, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
+dataloader_args = dict(shuffle=True, batch_size=16, num_workers=0, pin_memory=True) if cuda else dict(shuffle=True, batch_size=16)
 
 # train dataloader
 train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
